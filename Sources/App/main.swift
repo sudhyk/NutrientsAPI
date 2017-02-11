7e46d43a23b806fb355778fe99cc8a1c3f5309cd
Sudheendra Kumar Kaanugovi <sudhy.k@gmail.com>
2017-02-11 08:37:38 -0500

    Finished Project

--- Sources/App/main.swift
+++ Sources/App/main.swift
@@ -1,13 +1,14 @@
 import Vapor
 
+// The Droplet is a core class that gives you access to many of Vapor's facilities. It is responsible for registering routes, starting the server, appending middleware, and more.
 let drop = Droplet()
 
-drop.get { req in
-    return try drop.view.make("welcome", [
-    	"message": drop.localization[req.lang, "welcome", "title"]
-    ])
+// Create a new route to accept GET http method on "listofnutrients"
+// registers GET on URL http://localhost:8080/listofnutrients
+
+drop.get("listofnutrients") { req in
+    return try JSON(node: [ "nutrients" : ["Carbohydrates", "Protein", "Fat", "Vitamins","Minerals", "Water" ]])
 }
 
-drop.resource("posts", PostController())
-
+// After registering routes, now run the droplet
 drop.run()
